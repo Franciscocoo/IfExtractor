3f08e35e44898bf7863cf05e5c2c8711c051bdd6
Franciscocoo <francois.jullion99@gmail.com>
2021-06-08 13:44:50 +0200

    Add apk and output folder / Testing soot

--- src/main/java/analyseSoot/analyse/MainSoot.java
+++ src/main/java/analyseSoot/analyse/MainSoot.java
@@ -1,5 +1,22 @@
 package analyseSoot.analyse;
 
-public class MainSoot {
+import soot.options.Options;
 
+public class MainSoot {
+	
+	static String home = System.getProperty("user.home");
+	static String directory = System.getProperty("user.dir");
+	private static String repAndroid = home + "/Android/Sdk/platforms";
+	private static String apkName = "app-debug";
+	
+	static void initSoot() {
+		Options.v().set_allow_phantom_refs(true);
+		/* TODO : Add Options */
+	}
+	
+	public static void main(String[] args) {
+		// apkName = args[1];
+		System.out.println(System.getProperty("user.home"));
+		System.out.println(System.getProperty("user.dir"));
+	}
 }
