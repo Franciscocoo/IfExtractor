e73581e8fc141cc1d93a9e84c9bd1945c992020c
Franciscocoo <francois.jullion99@gmail.com>
2021-06-18 11:37:06 +0200

    Adding If isolator to mainSoot

--- src/main/java/analyseSoot/analyse/MainSoot.java
+++ src/main/java/analyseSoot/analyse/MainSoot.java
@@ -1,24 +1,36 @@
 package analyseSoot.analyse;
 
-import soot.options.Options;
-import soot.toolkits.graph.ExceptionalUnitGraph;
-import soot.toolkits.graph.UnitGraph;
-import soot.util.Chain;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+import java.util.stream.Collectors;
+
+import analyseSoot.utils.utils;
 import soot.Body;
+import soot.G;
+import soot.Local;
 import soot.PackManager;
+import soot.PatchingChain;
 import soot.Scene;
 import soot.SceneTransformer;
 import soot.SootClass;
 import soot.SootMethod;
 import soot.Transform;
 import soot.Unit;
-
-import analyseSoot.utils.*;
-
-import java.io.IOException;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Map;
+import soot.jimple.AbstractStmtSwitch;
+import soot.jimple.IfStmt;
+import soot.jimple.Stmt;
+import soot.options.Options;
+import soot.toolkits.graph.DirectedGraph;
+import soot.toolkits.graph.DominatorsFinder;
+import soot.toolkits.graph.ExceptionalUnitGraph;
+import soot.toolkits.graph.MHGDominatorsFinder;
+import soot.toolkits.graph.SimpleDominatorsFinder;
+import soot.toolkits.graph.MHGPostDominatorsFinder;
+import soot.toolkits.graph.UnitGraph;
+import soot.util.Chain;
 
 public class MainSoot {
 
@@ -27,7 +39,7 @@ public class MainSoot {
 	private static String repAndroid = home + "/Android/Sdk/platforms";
 	/* */
 	private static String directory = System.getProperty("user.dir");
-	private static String apkName = "app-debug";
+	private static String apkName = "instagram";
 	private static String repApk = directory + "/apk/" + apkName + ".apk";
 	/* */
 	private static String repOutput = directory + "/output";
@@ -36,6 +48,7 @@ public class MainSoot {
 	 * 
 	 */
 	static void initSoot() {
+		G.v().reset();
 		Options.v().set_allow_phantom_refs(true);
 		Options.v().set_android_jars(repAndroid);
 		List<String> l = new ArrayList<String>();
@@ -44,14 +57,14 @@ public class MainSoot {
 		Options.v().set_src_prec(Options.src_prec_apk);
 		Options.v().set_whole_program(true);
 		Scene.v().loadNecessaryClasses();
-		/* TODO : Add Options */
+		Scene.v().loadBasicClasses();
 	}
 
 	private static boolean isSystemClass(String className) {
 		return (className.startsWith("android.") || className.startsWith("java.") || className.startsWith("javax.") || 
 				className.startsWith("sun.") || className.startsWith("org.omg.") || className.startsWith("org.w3c.dom.") ||
 				className.startsWith("com.google.") || className.startsWith("com.android.")) || className.startsWith("androidx") ||
-				className.startsWith("com.example.demo.R");
+				className.startsWith("com.example.demo.R"); //Dernière à supprimer
 	}
 	
 	public static void main(String[] args) {
@@ -62,36 +75,85 @@ public class MainSoot {
 				Chain<SootClass> appClasses = Scene.v().getApplicationClasses();
 				for(SootClass c : appClasses) {
 					if(!isSystemClass(c.getName())) {
-						try {
-							System.out.println(c.getName());
-							utils.stringToJimple(c);
-						} catch (IOException e) {
-							e.printStackTrace();
+						System.out.println(c.getName());
+						for(SootMethod m : c.getMethods()) {
+							if(m.isConcrete()) {
+								Body b = m.retrieveActiveBody();
+								System.out.println(b);
+								for(Unit u : b.getUnits()) {
+									Stmt s = (Stmt) u;
+									if(s instanceof IfStmt) {
+										System.out.println(c);
+										System.out.println(m.getSignature());
+										//isolate(m, (IfStmt)s);
+										try {
+											utils.stringToJimple(c);
+										} catch (IOException e) {
+											System.out.println(e);
+										}
+									}
+								}
+							}
 						}
-						
+						System.out.println("**********");
 					}
 				}
 			}
 		}));
 		PackManager.v().runPacks();
 	}
+	
+	public static void saveJimple(Chain<SootClass> appClasses) {
+		for(SootClass c : appClasses) {
+			if(!isSystemClass(c.getName())) {
+				try {
+					System.out.println(c.getName());
+					utils.stringToJimple(c);
+				} catch (IOException e) {
+					e.printStackTrace();
+				}
+			}
+		}
+	}
+	
+	public static void isolate(SootMethod m, IfStmt stmt) {
+		/* Getting Data and grapj from Method m*/
+		Body b = m.retrieveActiveBody();
+		UnitGraph ug = new ExceptionalUnitGraph(b);
+		SimpleDominatorsFinder sdf = new SimpleDominatorsFinder(ug);
+		DirectedGraph dg = sdf.getGraph();
+		/* Getting the statement of right and left branch */
+		Stmt left = (Stmt) dg.getSuccsOf(stmt).get(0);
+		Stmt right = (Stmt) dg.getSuccsOf(stmt).get(1);
+		List<Stmt> branchLeft = new ArrayList<Stmt>();
+		branchLeft = getBranch(dg, branchLeft, left);
+		List<Stmt> branchRight = new ArrayList<Stmt>();
+		branchRight = getBranch(dg, branchRight, right);
+		System.out.println("Branch left");
+		System.out.println(branchLeft);
+		System.out.println("Branch Right");
+		System.out.println(branchRight);
+		List<Stmt> union = new ArrayList<Stmt>(branchLeft);
+		union.addAll(branchRight);
+		List<Stmt> intersection = new ArrayList<Stmt>(branchLeft);
+		intersection.retainAll(branchRight);
+		List<Stmt> res = new ArrayList<Stmt>(union);
+		res.removeAll(intersection);
+		System.out.println("ISOLATED IF :");
+		for(Stmt s: res) {
+			System.out.println(s);
+		}
+	}
+	
+	private static List<Stmt> getBranch(DirectedGraph dg, List<Stmt> l, Stmt s) {
+		l.add(s);
+		for(int i=0;i<dg.getSuccsOf(s).size();i++) {
+			Stmt succ = (Stmt) dg.getSuccsOf(s).get(i);
+			if(!l.contains(succ)) {
+				getBranch(dg, l, succ);
+			}
+		}
+		return l;
+	}
+	
 }
-
-/* Méthode de parcours du Graphe ?
-						System.out.println(c.getName());
-						List<SootMethod> classMethods = c.getMethods();
-						for(SootMethod m : c.getMethods()) {
-							Body b = m.retrieveActiveBody();
-							UnitGraph ug = new ExceptionalUnitGraph(b);
-							System.out.println("------");
-							System.out.println("Parcours du graphe de " + m.getName());
-							List<Unit> iterator = ug.getHeads();
-							System.out.println(iterator);
-							while(!iterator.equals(ug.getTails())) {
-								iterator = ug.getSuccsOf(iterator.get(0));
-								for(Unit s : iterator) {
-									System.out.println(iterator);
-								}
-							}
-							System.out.println("------");
-							/* Probleme sur mon parcours lorsque le code finit sur un if */
--- src/main/java/analyseSoot/utils/utils.java
+++ src/main/java/analyseSoot/utils/utils.java
@@ -4,9 +4,10 @@ import java.io.FileWriter;
 import java.io.IOException;
 import java.io.PrintWriter;
 
-import soot.SootClass;
+import soot.SootClass.*;
 import soot.SootMethod;
 import soot.Body;
+import soot.SootClass;
 
 public class utils {
 	
@@ -17,9 +18,12 @@ public class utils {
 		String filename = repOutput + "/" + cls.getName() + ".jmpl";
 		FileWriter filewrite = new FileWriter(filename);
 		PrintWriter printwrite = new PrintWriter(filewrite);
+		cls.setApplicationClass();
 		for(SootMethod meth: cls.getMethods()) {
-			Body b = meth.retrieveActiveBody();
-			printwrite.write(b.toString());
+			if(meth.isConcrete()) {
+				Body b = meth.retrieveActiveBody();
+				printwrite.write(b.toString());
+			}
 		}
 		printwrite.close();
 	}
